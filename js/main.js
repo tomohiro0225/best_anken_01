ece698a437709eb7a021340a0a6cc278e6858b56
hoshitetsu_web <9050h.6007f6006f@gmail.com>
2023-06-19 16:20:07 +0900

    メンバー部分、画像追加 slickでスライダー設定

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.member__img__lists').slick({
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    centerMode: true,
+    variableWidth: true,
+    dots: false,
+  });
