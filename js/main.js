d1768ea0d1302aae0c59bf3e6fa394ce3753708f
hoshitetsu_web <9050h.6007f6006f@gmail.com>
2023-06-20 16:01:05 +0900

    メンバー紹介を除いてコーディング完了

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$('.member__img__lists').slick({
-    autoplay: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    arrows: false,
-    centerMode: true,
-    variableWidth: true,
-    dots: false,
-  });
+// $('.member__img__lists').slick({
+//     autoplay: true,
+//     infinite: true,
+//     speed: 500,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     centerMode: true,
+//     variableWidth: true,
+//     dots: false,
+//   });
